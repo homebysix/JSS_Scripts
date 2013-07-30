e3297dd2d2b34ffc15177f08f740a069361008e2
JAMF Software <bram.cohen@jamfsoftware.com>
2013-07-30 13:18:27 -0500

    Update guestDisableEnable108.sh
    Added additional comment detail

--- guestDisableEnable108.sh
+++ guestDisableEnable108.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
-
-##########################################################################################
+#!/bin/sh
+############################################################################################################
+#
+# Copyright (c) 2013, JAMF Software, LLC.  All rights reserved.
+#
+#       Redistribution and use in source and binary forms, with or without
+#       modification, are permitted provided that the following conditions are met:
+#               * Redistributions of source code must retain the above copyright
+#                 notice, this list of conditions and the following disclaimer.
+#               * Redistributions in binary form must reproduce the above copyright
+#                 notice, this list of conditions and the following disclaimer in the
+#                 documentation and/or other materials provided with the distribution.
+#               * Neither the name of the JAMF Software, LLC nor the
+#                 names of its contributors may be used to endorse or promote products
+#                 derived from this software without specific prior written permission.
+#
 #       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
 #       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 #       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -12,12 +25,27 @@
 #       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-##########################################################################################
-# Based on: 
+############################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#  guestDisableEnable108.sh -- enables an account called Guest on the targeted system
+#
+# SYNOPSIS
+#	guestDisableEnable108.sh 
+#
+# DESCRIPTION
+#	This script does an enables a Guest user account if it does not exist or disables guest if it does exist.
+#
+############################################################################################################
+#
+# 2013-05-24--Created by Bram Cohen
+# Based largely on work done at the following sites:
 # https://github.com/nbalonso/Some_scripts/blob/master/Pushing%20Guest%20account/postflight.sh
 # https://github.com/sheagcraig/guestAccount/blob/master/guest_account
 #
-##########################################################################################
+############################################################################################################
 
 ##########################################################################################
 # Variables
